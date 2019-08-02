35b3feb8efd5e396de687b9c4fdb17d25fe42bf6
Mahendra Yadav <mahendra.yadav@go-mmt.com>
2019-08-03 00:40:17 +0530

    Write a func getModelArray that take data and convert it model array (this array is data source of collection view)

--- FlickrApp/HomeModule/HomeInteractor.swift
+++ FlickrApp/HomeModule/HomeInteractor.swift
@@ -17,4 +17,19 @@ extension HomeInteractor : HomePresenterToInteractorProtocol{
     func searchImageFromService(withText query:String, page:Int, pageCount:Int){
         
     }
+    
+    /// return an array of FlickrModel.
+    ///
+    /// - Parameters:
+    ///   - data: The data has to convert in model Array.
+    /// - Returns: A FlickrModel Array for the search result.
+    private func getModelArray(fromData data:Data)->[FlickrPhoto]?{
+        do{
+            let flickrModelArray = try JSONDecoder().decode([FlickrPhoto].self, from: data)
+            return flickrModelArray
+        }catch{
+            print(error)
+        }
+        return nil
+    }
 }
