7dfeb4bd2f0a3d06226868986da638b717642bbb
Mahendra Yadav <mahendra.yadav@go-mmt.com>
2019-08-02 23:50:43 +0530

    Create model for Flickr josn data using decodable property

--- FlickrApp/HomeModule/HomeEntity.swift
+++ FlickrApp/HomeModule/HomeEntity.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+
+/// Search result returned when searching images using `Flickr API service`.
+struct FlickrPhotoSearchResult: Decodable {
+    /// The page that was retrieved.
+    var page: Int
+    
+    /// The number of pages that exist for the search that was done.
+    var pages: Int
+    
+    /// The photos for the currently retrieved page.
+    var photos: [FlickrPhoto]
+    
+    private enum CodingKeys: String, CodingKey {
+        case page
+        case pages
+        case photos = "photo"
+    }
+}
+
+/// Wrapper for parsing the result from Flickr.
+struct FlickrPhotoSearchResultWrapper: Decodable {
+    var result: FlickrPhotoSearchResult
+    
+    private enum CodingKeys: String, CodingKey {
+        case result = "photos"
+    }
+}
+
+
+/// Photos returned when searching on Flickr.
+struct FlickrPhoto: Decodable {
+    
+    // MARK: - Properties
+    /// The id of the image.
+    var id: String
+    /// The title of the image.
+    var title: String
+    
+    // MARK: - Image URL Properties
+    // The following properties are used to create the url for retrieving the image.
+    var farm: Int
+    var secret: String
+    var server: String
+}
