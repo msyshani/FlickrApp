07e7e37ed5d15ec0faf95199dfebb3f5223ffa5d
Mahendra Yadav <mahendra.yadav@go-mmt.com>
2019-08-03 01:14:55 +0530

    Create APIRequest and URLEncoder class

---
+++ FlickrApp/Networking/APIRequests.swift
@@ -0,0 +1,13 @@
+//
+//  APIRequest.swift
+//  FlickrApp
+//
+//  Created by Mahendra Yadav on 03/08/19.
+//  Copyright © 2019 AppEngineer. All rights reserved.
+//
+
+import UIKit
+
+class APIRequests {
+
+}
---
+++ FlickrApp/Networking/URLEncoder.swift
@@ -0,0 +1,13 @@
+//
+//  URLEncoder.swift
+//  FlickrApp
+//
+//  Created by Mahendra Yadav on 03/08/19.
+//  Copyright © 2019 AppEngineer. All rights reserved.
+//
+
+import UIKit
+
+class URLEncoder {
+
+}
